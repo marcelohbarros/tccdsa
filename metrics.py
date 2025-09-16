f15584efe875e7db6c2460c95ba38b67730ac2c9
marcelohbarros <marcelo10037@hotmail.com>
2025-09-16 01:33:02 -0300

    adding name attribute in the metrics

--- metrics.py
+++ metrics.py
@@ -10,8 +10,9 @@ class Metric:
         if cls.all_metrics is None:
             cls.all_metrics = []
 
-        description = args[0]
-        tags = [tag.lower() for tag in args[1]]
+        name = args[0]
+        description = args[1]
+        tags = [tag.lower() for tag in args[2]]
 
         for tag in tags:
             if tag in cls.all_tags:
@@ -20,7 +21,7 @@ class Metric:
         cls.all_tags.update({tag.lower() for tag in tags})
 
         obj = super().__new__(cls)
-        obj.__init__(description, tags)
+        obj.__init__(name, description, tags)
         cls.all_metrics.append(obj)
         return obj
 
@@ -31,9 +32,14 @@ class Metric:
             if tag in m.tags:
                 return m
 
-    def __init__(self, description, tags):
+    def __init__(self, name, description, tags):
+        self._name = name
         self._tags = {tag.lower() for tag in tags}
         self._description = description
+
+    @property
+    def name(self):
+        return self._name
 
     @property
     def tags(self):
@@ -44,52 +50,52 @@ class Metric:
         return self._description
 
     def __repr__(self):
-        return f"Metric(\"{self._description}\", [\"{'", "'.join(self._tags)}\"])"
+        return f"Metric(\"{self._name}\", \"{self._description}\", [\"{'", "'.join(self._tags)}\"])"
 
     def __hash__(self):
-        return hash((", ".join(self._tags),))
+        return hash((self._name.upper(),))
 
 # Obligatory columns
-NAME = Metric("Name", ["name", "SHA"])
-BUG = Metric("Bug count", ["bug", "defect"])
+NAME = Metric("NAME", "Name", ["name", "SHA"])
+BUG = Metric("BUG", "Bug count", ["bug", "defect"])
 
 # Metrics
-AMC = Metric("Average Method Complexity", ["amc"])
-ASSIGNMENT_COUNT = Metric("Number of Assignments", ["assignmentsQty"])
-CA = Metric("Afferent Coupling", ["ca"])
-CAM = Metric("Cohesion Among Methods", ["cam"])
-CBM = Metric("Coupling Between Methods", ["cbm"])
-CBO = Metric("Coupling Between Objects/CountClassCoupled", ["cbo"])
-CC_AVG = Metric("Average Cyclomatic Complexity", ["avg_cc"])
-CC_MAX = Metric("Maximum Cyclomatic Complexity", ["max_cc"])
-CE = Metric("Efferent Couplings", ["ce"])
-COMPARISON_COUNT = Metric("Number of Comparisons", ["comparisonsQty"])
-DAM = Metric("Data Access Method", ["dam"])
-DIT = Metric("Depth of Inheritance Tree", ["dit"])
-FIELD_COUNT = Metric("Number of Fields", ["totalFields"])
-IC = Metric("Inheritance Coupling", ["ic"])
-LCOM = Metric("Lack of Cohesion in Methods", ["lcom"])
-LCOM3 = Metric("Lack of Cohesion in Methods 3", ["lcom3"])
-LOC = Metric("Lines of Code", ["loc", "loc_total", "iocode", "nloc", "countLineCode", "sloc", "nocl", "number_of_lines"])
-LOOP_COUNT = Metric("Number of Loops", ["loopQty"])
-MATH_OPERATION_COUNT = Metric("Number of Math Operations", ["mathOperationsQty"])
-METHOD_COUNT = Metric("Number of Methods", ["nom", "totalMethods", "numberOfMethods"])
-MFA = Metric("Measure of Functional Abstraction", ["mfa"])
-MOA = Metric("Measure of Aggregation", ["moa"])
-NESTED_BLOCKS_MAX = Metric("Maximum Nested Blocks", ["maxNestedBlocks"])
-NOC = Metric("Number of Children", ["noc", "nsub"])
-NPM = Metric("Number of Public Methods / Class Interface Size", ["npm", "nopm", "cis", "npbm"])
-NUMERIC_LITERAL_COUNT = Metric("Number of Numeric Literals", ["numbersQty"])
-PARENTHESIS_EXPRESSION_COUNT = Metric("Number of Parenthesis Expressions", ["parenthesizedExpsQty"])
-RETURN_COUNT = Metric("Number of Returns", ["returnsQty"])
-RFC = Metric("Response for a Class", ["rfc"])
-STATIC_INVOCATION_COUNT = Metric("Number of Static Invocations", ["nosi"])
-STRING_LITERAL_COUNT = Metric("Number of String Literals", ["stringLiteralsQty"])
-TRY_CATCH_COUNT = Metric("Number of Try-Catch Blocks", ["tryCatchQty"])
-UNIQUE_WORD_COUNT = Metric("Number of Unique Words", ["uniqueWordsQty"])
-VARIABLE_COUNT = Metric("Number of Variables", ["variablesQty"])
-VERSION = Metric("Project version", ["version"])
-WMC = Metric("Weighted Methods per Class", ["wmc"])
+AMC = Metric("AMC", "Average Method Complexity", ["amc"])
+ASSIGNMENT_COUNT = Metric("ASSIGNMENT_COUNT", "Number of Assignments", ["assignmentsQty"])
+CA = Metric("CA", "Afferent Coupling", ["ca"])
+CAM = Metric("CAM", "Cohesion Among Methods", ["cam"])
+CBM = Metric("CBM", "Coupling Between Methods", ["cbm"])
+CBO = Metric("CBO", "Coupling Between Objects/CountClassCoupled", ["cbo"])
+CC_AVG = Metric("CC_AVG", "Average Cyclomatic Complexity", ["avg_cc"])
+CC_MAX = Metric("CC_MAX", "Maximum Cyclomatic Complexity", ["max_cc"])
+CE = Metric("CE", "Efferent Couplings", ["ce"])
+COMPARISON_COUNT = Metric("COMPARISON_COUNT", "Number of Comparisons", ["comparisonsQty"])
+DAM = Metric("DAM", "Data Access Method", ["dam"])
+DIT = Metric("DIT", "Depth of Inheritance Tree", ["dit"])
+FIELD_COUNT = Metric("FIELD_COUNT", "Number of Fields", ["totalFields"])
+IC = Metric("IC", "Inheritance Coupling", ["ic"])
+LCOM = Metric("LCOM", "Lack of Cohesion in Methods", ["lcom"])
+LCOM3 = Metric("LCOM3", "Lack of Cohesion in Methods 3", ["lcom3"])
+LOC = Metric("LOC", "Lines of Code", ["loc", "loc_total", "iocode", "nloc", "countLineCode", "sloc", "nocl", "number_of_lines"])
+LOOP_COUNT = Metric("LOOP_COUNT", "Number of Loops", ["loopQty"])
+MATH_OPERATION_COUNT = Metric("MATH_OPERATION_COUNT", "Number of Math Operations", ["mathOperationsQty"])
+METHOD_COUNT = Metric("METHOD_COUNT", "Number of Methods", ["nom", "totalMethods", "numberOfMethods"])
+MFA = Metric("MFA", "Measure of Functional Abstraction", ["mfa"])
+MOA = Metric("MOA", "Measure of Aggregation", ["moa"])
+NESTED_BLOCKS_MAX = Metric("NESTED_BLOCKS_MAX", "Maximum Nested Blocks", ["maxNestedBlocks"])
+NOC = Metric("NOC", "Number of Children", ["noc", "nsub"])
+NPM = Metric("NPM", "Number of Public Methods / Class Interface Size", ["npm", "nopm", "cis", "npbm"])
+NUMERIC_LITERAL_COUNT = Metric("NUMERIC_LITERAL_COUNT", "Number of Numeric Literals", ["numbersQty"])
+PARENTHESIS_EXPRESSION_COUNT = Metric("PARENTHESIS_EXPRESSION_COUNT", "Number of Parenthesis Expressions", ["parenthesizedExpsQty"])
+RETURN_COUNT = Metric("RETURN_COUNT", "Number of Returns", ["returnsQty"])
+RFC = Metric("RFC", "Response for a Class", ["rfc"])
+STATIC_INVOCATION_COUNT = Metric("STATIC_INVOCATION_COUNT", "Number of Static Invocations", ["nosi"])
+STRING_LITERAL_COUNT = Metric("STRING_LITERAL_COUNT", "Number of String Literals", ["stringLiteralsQty"])
+TRY_CATCH_COUNT = Metric("TRY_CATCH_COUNT", "Number of Try-Catch Blocks", ["tryCatchQty"])
+UNIQUE_WORD_COUNT = Metric("UNIQUE_WORD_COUNT", "Number of Unique Words", ["uniqueWordsQty"])
+VARIABLE_COUNT = Metric("VARIABLE_COUNT", "Number of Variables", ["variablesQty"])
+VERSION = Metric("VERSION", "Project version", ["version"])
+WMC = Metric("WMC", "Weighted Methods per Class", ["wmc"])
 
 
 class ArgumentMetrics:
